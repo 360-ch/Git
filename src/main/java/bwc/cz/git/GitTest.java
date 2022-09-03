638f26c28eb468b33de49b92c426db0ee85f80a4
cz <cz@bwc.com>
2022-09-03 19:29:23 +0800

    second commit

--- src/main/java/bwc/cz/git/GitTest.java
+++ src/main/java/bwc/cz/git/GitTest.java
@@ -3,5 +3,6 @@ package bwc.cz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git1!");
     }
 }
